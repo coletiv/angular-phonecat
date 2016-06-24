85e7875e3f7df49d08cf660348f980bf0350f8df
Tiago Duarte <tmpduarte@gmail.com>
2016-06-24 16:55:20 +0200

    converted filter to use es6

--- app/core/core.module.js
+++ app/core/core.module.js
@@ -1,5 +1,8 @@
 import angular from 'angular';
 import phoneModule from './phone/phone.module.js';
+import checkMarkFilter from './checkmark/checkmark.filter.js';
 
 // Define the `core` module
-export default angular.module('core', [phoneModule]).name;
+export default angular.module('core', [phoneModule])
+.filter('checkmark', checkMarkFilter)
+.name;
